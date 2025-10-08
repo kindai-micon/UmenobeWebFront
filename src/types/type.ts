c47a4c16f8ad3b97fec74bc8ccec89391fcc6946
mamenz752 <mamenz752@gmail.com>
2025-10-08 23:27:01 +0900

    refac: adjust shop page to api

--- src/types/type.ts
+++ src/types/type.ts
@@ -5,9 +5,10 @@ export type TextItem = { [key: string]: string };
 export type ExhibitItem = { name: string; images: ImageItem[]; texts: TextItem[] };
 
 export type CorpItem = { name: string; images: ImageItem[]; texts: TextItem[] };
+export type ShopItem = { name: string; images: ImageItem[]; texts: TextItem[] };
 
 export type FileItem = { name: string; filename: string };
 
 export type MergedItem = { name: string; filename: string; text: string };
 
-export type ShopItem = { imageData: string; textData: string };
+export type ShopItema = { imageData: string; textData: string };
